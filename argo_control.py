9fd77b62f7bade7a9e7aa8fa35216781abe5f87d
solmoralessal-lang <sol.morales.sal@gmail.com>
2026-03-05 14:57:27 -0600

    Update argo_control.py

--- argo_control.py
+++ argo_control.py
@@ -79,12 +79,12 @@ if id_operacion:
 else:
     nombre_salida = f"ARGO_CONTROL_{estatus}_{fecha}.xlsx"
 
-output_path = os.path.join("outputs", nombre_salida)
-os.makedirs("outputs", exist_ok=True)
+    output_path = os.path.join("outputs", nombre_salida)
+    os.makedirs("outputs", exist_ok=True)
 
-wb_control.save(output_path)
+    wb_control.save(output_path)
 
-return output_path, icono, estatus
+    return output_path, icono, estatus
 
 def extraer_resumen_control_desde_excel(output_path: str) -> dict:
     """
